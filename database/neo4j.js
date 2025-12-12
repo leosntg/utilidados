6bebea51e1798066f478eaca5d831ca45bd1b249
Leonardo <leonardosntgf@gmail.com>
2025-12-12 16:36:27 -0300

    feat: adiciona variáveis de ambiente

--- database/neo4j.js
+++ database/neo4j.js
@@ -1,8 +1,12 @@
 const neo4j = require('neo4j-driver');
 
+const NEO4J_URI = process.env.NEO4J_URI || 'bolt://localhost:7687';
+const NEO4J_USER = process.env.NEO4J_USER || 'neo4j';
+const NEO4J_PASSWORD = process.env.NEO4J_PASSWORD || 'password';
+
 const driver = neo4j.driver(
-    'bolt://localhost:7687',
-    neo4j.auth.basic('neo4j', 'password')
+    NEO4J_URI,
+    neo4j.auth.basic(NEO4J_USER, NEO4J_PASSWORD)
 );
 
 module.exports = driver;
